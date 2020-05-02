521cffe55eff0fc6f45d01d6721d1948ae7216cf
Mohsen Samiei <eng.msamiee@gmail.com>
2020-05-03 01:00:48 +0430

    feat: implements example code

---
+++ examples/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"errors"
+	"github.com/golage/log"
+)
+
+type data struct {
+	Name string
+}
+
+func main() {
+	log.SetLevel(log.LevelDebug)
+	log.SetConstant("code_name", "example")
+
+	log.Value("name", "john").Debug("debug message")
+
+	log.With("john").Info("info message")
+
+	log.With(data{Name: "john"}).Warning("warning message")
+
+	log.With(errors.New("message")).Error("error message")
+
+	log.Fatal("fatal error")
+}
